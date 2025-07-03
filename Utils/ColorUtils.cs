791c4de9f5b5d43875cc453d31c416d683319257
mdomnik <mdomnik.maciej@gmail.com>
2025-07-03 22:52:15 +0200

    ColorUtils commented

--- Utils/ColorUtils.cs
+++ Utils/ColorUtils.cs
@@ -4,6 +4,7 @@ namespace ImageColorQuantizer.Utils
 {
     internal class ColorUtils
     {
+        // helper method to convert a list of Color objects to a list of RGB tuples
         public static List<(int R, int G, int B)> ConvertToRGBTuples(List<Color> colors)
         {
             return colors.Select(c => ((int)c.R, (int)c.G, (int)c.B)).ToList();
